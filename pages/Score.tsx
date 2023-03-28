bd8be444572339467bff0f5c02059dd71aef1bbc
Juan Vélez López <velezlopezjuan1@gmail.com>
2023-03-29 01:33:32 +0200

    Buffed manu's value to be that of 5 🔥

--- pages/Score.tsx
+++ pages/Score.tsx
@@ -6,8 +6,8 @@ interface IScoreProps {
 
 export default function Score({ streak }: IScoreProps) {
   if (!streak) return <></>;
-  return <div className="flex flex-wrap gap-4 justify-center">
-    {Array(Math.floor(streak / 5)).fill(0).map((_, index) => <span className="text-xl" key={index} role="img" aria-label="fueguito">🔥</span>)}
-    {Array(streak % 5).fill(0).map((_, index) => <Image src="/manu.jpg" alt="manu" width="32" height="32"/>)}
+  return <div className="flex flex-wrap gap-4 justify-center items-center">
+    {Array(Math.floor(streak / 5)).fill(0).map((_, index) => <Image src="/manu.jpg" key={index} alt="manu" width="48" height="48"/>)}
+    {Array(Math.floor(streak % 5)).fill(0).map((_, index) => <span className="text-4xl" key={index} role="img" aria-label="fueguito">🔥</span>)}
   </div>;
 }
