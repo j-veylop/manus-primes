9e9b4fd28ef84e34ad9e0662364e447137be4e0d
Juan Vélez López <velezlopezjuan1@gmail.com>
2023-03-28 21:59:15 +0200

    Improved mobile view

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,6 +37,8 @@ export default function Home() {
   const [wrongAnswers, setWrongAnswers] = useState(0);
   const [totalPrimes, setTotalPrimes] = useState(0);
 
+  const [numberList, setNumberList] = useState<number[]>([]);
+
   const isPrime = (num: number) => {
     return primes.includes(num);
   }
@@ -48,6 +50,7 @@ export default function Home() {
 
   useEffect(() => {
     if (answer !== null) {
+      setNumberList([...numberList, num].sort((a, b) => (a - b)));
       if (isPrime(num)) setTotalPrimes(totalPrimes + 1);
 
       if (answer === isPrime(num)) {
@@ -64,13 +67,13 @@ export default function Home() {
   }, [answer]);
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen text-4xl gap-32 px-20 py-16">
+    <div className="flex flex-col items-center justify-center min-h-screen text-4xl text-center gap-12 px-12 py-16">
       <p>Is {num} prime?</p>
       <div className="flex flex-row gap-4 text-white">
         <button className="bg-[#936b79] rounded-md p-4 w-24" onClick={() => setAnswer(true)}>Yes</button>
         <button className="bg-[#936b79] rounded-md p-4 w-24" onClick={() => setAnswer(false)}>No</button>
       </div>
-      <div className="flex flex-col gap-4 text-xl text-center">
+      <div className="flex flex-col gap-4 text-sm text-center">
         <p>Right primes guessed: {rightPrimes}</p>
         <p>Total primes: {totalPrimes}</p>
         <p>Right answers: {rightAnswers}</p>
@@ -79,6 +82,12 @@ export default function Home() {
         <p>Percentage of primes guessed correctly: {(100 * rightPrimes / totalPrimes || 0).toFixed(2)} %</p>
       </div>
       <iframe src="https://open.spotify.com/embed/album/0RcgQYkpfKAhg7dyoXoPm8?utm_source=generator" width="100%" height="352" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+      <div className="flex flex-col gap-2">
+        {numberList.map((num) => {
+          if (isPrime(num)) return <p className="text-xs font-bold">{num}</p>
+          else return <p className="text-xs">{num}</p>
+        })}
+      </div>
     </div>
   )
 }
