e77717798f60ed640da87d686a32c4c7ab454ee7
j-veylop <32459389+j-veylop@users.noreply.github.com>
2023-03-29 17:30:18 +0200

    Added streak info

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,6 +34,9 @@ export default function Home() {
   const [wrongAnswers, setWrongAnswers] = useState(0);
   const [totalPrimes, setTotalPrimes] = useState(0);
 
+  const [currentStreak, setCurrentStreak] = useState(0);
+  const [maxStreak, setMaxStreak] = useState(0);
+
   const [numberList, setNumberList] = useState<number[]>([]);
 
   function resetScore() {
@@ -81,6 +84,8 @@ export default function Home() {
     setRightAnswers(parseInt(localStorage.getItem('rightAnswers') || '0'));
     setWrongAnswers(parseInt(localStorage.getItem('wrongAnswers') || '0'));
     setTotalPrimes(parseInt(localStorage.getItem('totalPrimes') || '0'));
+    setCurrentStreak(parseInt(localStorage.getItem('currentStreak') || '0'));
+    setMaxStreak(parseInt(localStorage.getItem('maxStreak') || '0'));
 
     let numList: number[] = [];
     if (localStorage.numberList) {
@@ -121,6 +126,12 @@ export default function Home() {
       }
 
       if (answer === isPrime(num)) {
+        localStorage.currentStreak = currentStreak + 1;
+        setCurrentStreak(currentStreak => currentStreak + 1);
+        if (currentStreak > maxStreak) {
+          localStorage.maxStreak = maxStreak + 1;
+          setMaxStreak(maxStreak => maxStreak + 1)
+        }
         if (isPrime(num)) {
           localStorage.rightPrimes = rightPrimes + 1;
           setrightPrimes(rightPrimes => rightPrimes + 1);
@@ -129,6 +140,8 @@ export default function Home() {
         setRightAnswers(rightAnswers => rightAnswers + 1);
         alert(`Correct 👌, ${num} is ${isPrime(num) ? "" : "not "}prime!`);
       } else {
+        localStorage.currentStreak = 0;
+        setCurrentStreak(0);
         localStorage.wrongAnswers = wrongAnswers + 1;
         setWrongAnswers(wrongAnswers => wrongAnswers + 1);
         alert(`Wrong ❌, ${num} is ${isPrime(num) ? "" : "not "}prime!`);
@@ -146,6 +159,8 @@ export default function Home() {
         <button className="bg-dune-alive rounded-md p-4 w-full" onClick={() => answerQuestion(false)}>No</button>
       </div>
       <div className="flex flex-col gap-4 text-sm text-center">
+        <p>Current Streak: {currentStreak}</p>
+        <p>Max Streak: {maxStreak}</p>
         <p>Right primes guessed: {rightPrimes}</p>
         <p>Total primes: {totalPrimes}</p>
         <p>Right answers: {rightAnswers}</p>
